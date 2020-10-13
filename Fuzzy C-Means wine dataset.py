9140909cd7bb9fde01eb29f2ef45a63672d9f41d
Samuel Oliveira <samuelcpoliveira@gmail.com>
2020-10-12 21:51:47 -0300

    Adicionados loops para verificar demais condições e valores de variáveis

--- Fuzzy C-Means wine dataset.py
+++ Fuzzy C-Means wine dataset.py
@@ -5,7 +5,9 @@ Created on Sat Sep 19 19:47:14 2020
 @author: Marco Antonio, Samuel e Jorge
 """
 
+# %%
 from __future__ import division, print_function
+import pandas as pd
 import skfuzzy as fuzz
 from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split
@@ -13,41 +15,49 @@ import numpy as np
 
 #Carrega o wine dataset em wines
 wines = load_wine()
+m_base = [1.5, 2, 2.5, 3, 3.5, 4, 6, 8, 10]
+# m_base = [4]
 
-#Define os parâmetros para o treinamento e teste
-ncenters = 4
-m = 2
-numero_de_testes = 100
+# %%
 
-#Inicializações
-acertos = 0
+dataset = pd.DataFrame(np.column_stack((wines.data, wines.target)),
+                   columns = wines.feature_names + ['Target'])
+print(dataset)
 
-#Dividindo o dataset em treinamento e checagem
-wine_train, wine_test, label_train, label_test = train_test_split(
-    wines.data,wines.target,test_size=0.50,random_state=123)
+# %%
 
-#Loop para realizar o processo de treinamento e checagem X vezes
-
-for x in range(numero_de_testes):
-
-    #Implementa o Algoritmo Fuzzy C-means
-    cntr, u, u0, d, jm, p, fpc = fuzz.cluster.cmeans(
-            wine_train.transpose(), ncenters, m, error=0.005, maxiter=1000, init=None)
-    cluster_membership = np.argmax(u, axis=0)
-
-    #A partir dos centros gerados testa o dataset
-    u, u0, d, jm, p, fpc = fuzz.cluster.cmeans_predict(
-            wine_test.transpose(), cntr, m, error=0.005, maxiter=1000)
-    cluster_membership = np.argmax(u, axis=0)
-
-    #Confere com a label e conclui a quantidade de acertos real do Algorítmo
-    for x in range(len(cluster_membership)):
-        if cluster_membership[x] == label_test[x]:
-            acertos += 1
-
-acertos /= len(cluster_membership) * numero_de_testes
+for i in m_base:
+    #Define os parâmetros para o treinamento e teste
+    ncenters = 3
+    m = i
+    numero_de_testes = 100
     
+    #Inicializações
+    acertos = 0
     
+    #Dividindo o dataset em treinamento e checagem
+    wine_train, wine_test, label_train, label_test = train_test_split(
+        wines.data,wines.target,test_size=0.50,random_state=123)
     
+    #Loop para realizar o processo de treinamento e checagem X vezes
     
+    for _ in range(numero_de_testes):
     
+        #Implementa o Algoritmo Fuzzy C-means
+        cntr, u, u0, d, jm, p, fpc = fuzz.cluster.cmeans(
+                wine_train.transpose(), ncenters, m, error=0.005, maxiter=1000, init=None)
+        cluster_membership = np.argmax(u, axis=0)
+    
+        #A partir dos centros gerados testa o dataset
+        u, u0, d, jm, p, fpc = fuzz.cluster.cmeans_predict(
+                wine_test.transpose(), cntr, m, error=0.005, maxiter=1000)
+        cluster_membership = np.argmax(u, axis=0)
+    
+        #Confere com a label e conclui a quantidade de acertos real do Algorítmo
+        for x in range(len(cluster_membership)):
+            if cluster_membership[x] == label_test[x]:
+                acertos += 1
+    
+    acertos /= len(cluster_membership) * numero_de_testes
+    
+    print('acertos com m = ', str(i), ': ', acertos*100, '%')
--- KNN wine dataset.py
+++ KNN wine dataset.py
@@ -5,34 +5,46 @@ Created on Sat Sep 19 19:47:14 2020
 @author: Marco Antonio, Samuel e Jorge
 """
 
+# %%
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split
  
 #Carrega o wine dataset em wines 
 wines = load_wine()
+vizinhos = 10
 
-#Inicialização
-acertos = 0
-numero_de_testes = 100
-numero_de_vizinhos = 10
+# %%
 
-#Dividindo o dataset em treinamento e checagem
-wine_train, wine_test, label_train, label_test = train_test_split(
-    wines.data,wines.target,test_size=0.5,random_state=123)
+dataset = pd.DataFrame(np.column_stack((wines.data, wines.target)),
+                   columns = wines.feature_names + ['Target'])
+print(dataset)
 
-#Loop para realização das simulações
-for x in range(numero_de_testes):
+# %%
+for i in range(1, vizinhos+1):
+    #Inicialização
+    acertos = 0
+    numero_de_testes = 100
+    numero_de_vizinhos = i
     
-    #Implementa o Algoritmo KNN
-    neigh = KNeighborsClassifier(n_neighbors=numero_de_vizinhos,weights="uniform")
-    neigh.fit(wine_train, label_train)
+    #Dividindo o dataset em treinamento e checagem
+    wine_train, wine_test, label_train, label_test = train_test_split(
+        wines.data,wines.target,test_size=0.5,random_state=123)
     
-    #Prevendo novos valores
-    previsao = neigh.predict(wine_test)
+    #Loop para realização das simulações
+    for x in range(numero_de_testes):
+        
+        #Implementa o Algoritmo KNN
+        neigh = KNeighborsClassifier(n_neighbors=numero_de_vizinhos,weights="distance")
+        neigh.fit(wine_train, label_train)
+        
+        #Prevendo novos valores
+        previsao = neigh.predict(wine_test)
+        
+        for x in range(len(previsao)):
+            if previsao[x] == label_test[x]:
+                acertos += 1
+                
+    acertos /= len(previsao) * numero_de_testes
     
-    for x in range(len(previsao)):
-        if previsao[x] == label_test[x]:
-            acertos += 1
-            
-acertos /= len(previsao) * numero_de_testes
+    print('acertos com ', str(i), 'vizinhos: ', acertos*100, '%')
